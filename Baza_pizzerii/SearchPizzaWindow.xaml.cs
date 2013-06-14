d35f8ef9ffe3d37155dd6c4ff521636b88af00ea
Waszak <kwaszak67@wp.pl>
2013-06-14 20:02:11 +0200

    Lista składników dynamicznie.

--- Baza_pizzerii/SearchPizzaWindow.xaml.cs
+++ Baza_pizzerii/SearchPizzaWindow.xaml.cs
@@ -10,28 +10,39 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
-namespace Baza_pizzerii
-{
+namespace Baza_pizzerii {
     /// <summary>
     /// Interaction logic for SearchPizzaWindow.xaml
     /// </summary>
-    public partial class SearchPizzaWindow : Window
-    {
-        public SearchPizzaWindow()
-        {
-            InitializeComponent();
+
+    public class Ingredient {
+        public Ingredient() {
         }
 
-        private void myAccount_Click(object sender, RoutedEventArgs e)
-        {
-            if (App.Current.Properties["rola"].ToString() == "gosc")
-            {
+        public string Name { get; set; }
+    }
+
+    public partial class SearchPizzaWindow : Window {
+        public SearchPizzaWindow() {
+            InitializeComponent();
+            ObservableCollection<Ingredient> ingredients = new ObservableCollection<Ingredient>
+                                               {
+                                                   new Ingredient {Name = "szynka"},
+                                                   new Ingredient {Name = "pieczarki"},
+                                                   new Ingredient {Name = "gyros"},
+                                                   new Ingredient {Name = "papryka"}
+                                               };
+            this.menuIngredients.ItemsSource = ingredients;
+        }
+
+        private void myAccount_Click(object sender, RoutedEventArgs e) {
+            if (App.Current.Properties["rola"].ToString() == "gosc") {
                 MessageBox.Show("Korzystasz z aplikacji jako gość.\nFunkcjonalność dostępna dla zalogowanych użytkowników.");
                 return;
             }
-            if (App.Current.Properties["rola"].ToString() == "klient")
-            {
+            if (App.Current.Properties["rola"].ToString() == "klient") {
                 //testowo na razie tylko wyswietlamy wszystko globalne zmienne
                 MessageBox.Show("Login: " + App.Current.Properties["login"].ToString() +
                                 "\nId_osoba: " + App.Current.Properties["id_osoba"].ToString() +
@@ -42,8 +53,7 @@ namespace Baza_pizzerii
             }
         }
 
-        private void searchPizzeria_Click(object sender, RoutedEventArgs e)
-        {
+        private void searchPizzeria_Click(object sender, RoutedEventArgs e) {
             var loginWindow = new SearchPizzeriaWindow();
             loginWindow.Top = this.Top;
             loginWindow.Left = this.Left;
@@ -51,8 +61,7 @@ namespace Baza_pizzerii
             this.Close();
         }
 
-        private void searchPizza_Click(object sender, RoutedEventArgs e)
-        {
+        private void searchPizza_Click(object sender, RoutedEventArgs e) {
             var loginWindow = new SearchPizzaWindow();
             loginWindow.Top = this.Top;
             loginWindow.Left = this.Left;
@@ -60,8 +69,7 @@ namespace Baza_pizzerii
             this.Close();
         }
 
-        private void logout_Click(object sender, RoutedEventArgs e)
-        {
+        private void logout_Click(object sender, RoutedEventArgs e) {
             var loginWindow = new LoginWindow();
             loginWindow.Top = this.Top;
             loginWindow.Left = this.Left;
