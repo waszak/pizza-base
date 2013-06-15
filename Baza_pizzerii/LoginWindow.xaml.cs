ba167f8f03b70a7ade18a5dfa029a0e0995f1a45
Waszak <kwaszak67@wp.pl>
2013-06-15 15:01:10 +0200

    Hide navigation bar from LoginWindow and remove unused Imports

--- Baza_pizzerii/LoginWindow.xaml.cs
+++ Baza_pizzerii/LoginWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Npgsql;
 
 namespace Baza_pizzerii
@@ -20,7 +8,7 @@ namespace Baza_pizzerii
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : Window 
     {
         public LoginWindow()
         {
