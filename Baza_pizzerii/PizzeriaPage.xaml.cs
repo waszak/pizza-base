7df148aac19b9f139d2bcfff01243a6f2b4bc0ae
Waszak <kwaszak67@wp.pl>
2013-07-01 20:18:20 +0200

    Add PizzeriaPage.

---
+++ Baza_pizzerii/PizzeriaPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Baza_pizzerii {
+    /// <summary>
+    /// Interaction logic for PizzeriaPage.xaml
+    /// </summary>
+    
+    public partial class PizzeriaPage : Page {
+        private string pizzeria_id;
+        public PizzeriaPage(string id) {
+            InitializeComponent();
+            this.pizzeria_id = id;
+        }
+        private void searchPizzeriaPage_Click(object sender, RoutedEventArgs e) {
+            this.NavigationService.RemoveBackEntry();
+            this.NavigationService.Navigate(new SearchPizzeriaPage());
+        }
+
+        private void searchPizzaPage_Click(object sender, RoutedEventArgs e) {
+            this.NavigationService.RemoveBackEntry();
+            this.NavigationService.Navigate(new SearchPizzaPage());
+        }
+        private void logout_Click(object sender, RoutedEventArgs e) {
+            this.NavigationService.RemoveBackEntry();
+            this.NavigationService.Navigate(new LoginPage());
+        }
+
+        private void myAccount_Click(object sender, RoutedEventArgs e) {
+            if (App.Current.Properties["rola"].ToString() == "gosc") {
+                MessageBox.Show("Korzystasz z aplikacji jako gość.\nFunkcjonalność dostępna dla zalogowanych użytkowników.");
+                return;
+            }
+            if (App.Current.Properties["rola"].ToString() == "klient") {
+                //testowo na razie tylko wyswietlamy wszystko globalne zmienne
+                MessageBox.Show("Login: " + App.Current.Properties["login"].ToString() +
+                                "\nId_osoba: " + App.Current.Properties["id_osoba"].ToString() +
+                                "\nRola: " + App.Current.Properties["rola"].ToString() +
+                                "\nImie: " + App.Current.Properties["imie"].ToString() +
+                                "\nNazwisko: " + App.Current.Properties["nazwisko"].ToString());
+                return;
+            }
+        }
+    }
+
+
+
+}
